8f6498b0ad982eef821e11d8527287d641dae261
maprost <matthias.leuthaeuser@gmx.de>
2017-06-29 21:53:38 +0200

    insert new server status

--- rctest/serverstatus.go
+++ rctest/serverstatus.go
@@ -17,6 +17,14 @@ func Status400() restclient.Result {
 	return Status(http.StatusBadRequest)
 }
 
+func Status401() restclient.Result {
+	return Status(http.StatusUnauthorized)
+}
+
+func Status402() restclient.Result {
+	return Status(http.StatusPaymentRequired)
+}
+
 func Status403() restclient.Result {
 	return Status(http.StatusForbidden)
 }
@@ -25,12 +33,40 @@ func Status404() restclient.Result {
 	return Status(http.StatusNotFound)
 }
 
+func Status405() restclient.Result {
+	return Status(http.StatusMethodNotAllowed)
+}
+
+func Status406() restclient.Result {
+	return Status(http.StatusNotAcceptable)
+}
+
+func Status407() restclient.Result {
+	return Status(http.StatusProxyAuthRequired)
+}
+
+func Status408() restclient.Result {
+	return Status(http.StatusRequestTimeout)
+}
+
+func Status409() restclient.Result {
+	return Status(http.StatusConflict)
+}
+
+func Status418() restclient.Result {
+	return Status(http.StatusTeapot)
+}
+
 func Status500() restclient.Result {
 	return Status(http.StatusInternalServerError)
 }
 
 func Status501() restclient.Result {
 	return Status(http.StatusNotImplemented)
+}
+
+func Status502() restclient.Result {
+	return Status(http.StatusBadGateway)
 }
 
 func Status503() restclient.Result {
--- rctest/serverstatus_test.go
+++ rctest/serverstatus_test.go
@@ -10,10 +10,21 @@ func TestStatus(t *testing.T) {
 
 	assert.Equal(Status200().StatusCode, 200)
 	assert.Equal(Status204().StatusCode, 204)
+
 	assert.Equal(Status400().StatusCode, 400)
+	assert.Equal(Status401().StatusCode, 401)
+	assert.Equal(Status402().StatusCode, 402)
 	assert.Equal(Status403().StatusCode, 403)
 	assert.Equal(Status404().StatusCode, 404)
+	assert.Equal(Status405().StatusCode, 405)
+	assert.Equal(Status406().StatusCode, 406)
+	assert.Equal(Status407().StatusCode, 407)
+	assert.Equal(Status408().StatusCode, 408)
+	assert.Equal(Status409().StatusCode, 409)
+	assert.Equal(Status418().StatusCode, 418)
+
 	assert.Equal(Status500().StatusCode, 500)
 	assert.Equal(Status501().StatusCode, 501)
+	assert.Equal(Status502().StatusCode, 502)
 	assert.Equal(Status503().StatusCode, 503)
 }
